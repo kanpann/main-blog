ba4a0397681e7b49b16936a73e1856d4fa278348
kanpann <gunkim.dev@gmail.com>
2021-03-18 16:09:50 +0900

    게시글 Grid 조정

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -1,39 +1,56 @@
 import React from "react";
 import { Link } from "gatsby";
 import { TARGET_CLASS } from "../../utils/visible";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 import "./index.scss";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    maxHeight: 420,
+    display: "block",
+    margin: "12px",
+    padding: "4px",
+    paddingBottom: "12px",
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
+  media: {
+    height: 140,
   },
 }));
 
 export const ThumbnailItem = ({ node }) => {
   const classes = useStyles();
   return (
-    <Grid xs={12} lg={6}>
+    <Grid xs={12} md={6} lg={4}>
       <Link
         className={`thumbnail ${TARGET_CLASS}`}
         to={node.fields.slug.substr(17)}
       >
-        <Grid key={node.fields.slug} container spacing={3}>
-          <Grid item xs={12} md={5} lg={6}>
-            <img src={node.frontmatter.image} />
-          </Grid>
-          <Grid item xs={12} md={7} lg={6}>
-            <h3>{node.frontmatter.title || node.fields.slug}</h3>
-            <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-          </Grid>
-        </Grid>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={node.frontmatter.image}
+              title={node.frontmatter.title || node.fields.slug}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {node.frontmatter.title || node.fields.slug}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                component="div"
+                dangerouslySetInnerHTML={{ __html: node.excerpt }}
+              ></Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </Link>
     </Grid>
   );
