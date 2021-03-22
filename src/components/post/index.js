4e38a90d0f680d0764f8f3dae617aa1d7844208d
kanpann <45007556+kanpann@users.noreply.github.com>
2021-03-22 17:44:58 +0900

    컴포넌트 구조 롤백

--- src/components/post/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./comments";
-export * from "./post-container";
-export * from "./post-date";
-export * from "./post-navigator";
-export * from "./post-title";
-export * from "./social-share";
