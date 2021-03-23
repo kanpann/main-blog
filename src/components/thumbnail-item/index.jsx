80e94fa3e043668e6195c5bc5706f3e63ada9dc3
kanpann <gunkim.dev@gmail.com>
2021-03-23 13:59:47 +0900

    제목이 40바이트를 넘어갈 경우 자르도록 수정

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -2,36 +2,28 @@ import React from "react";
 import { Link } from "gatsby";
 import { TARGET_CLASS } from "../../utils/visible";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
 import "./index.scss";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxHeight: 420,
-    display: "block",
-    margin: "12px",
-    padding: "4px",
-    paddingBottom: "12px",
-  },
-  media: {
-    height: 140,
-  },
-}));
+function substrToByte(targetStr, maxByte) {
+  let buffer = 0;
+  let idx = 0;
+  while (true) {
+    const unicode = targetStr.charCodeAt(idx);
+    buffer += unicode > 127 ? 2 : 1;
+
+    if (buffer > maxByte) break;
+    idx++;
+  }
+  let result = targetStr.substring(0, idx);
+  if (idx < targetStr.length) {
+    result += "...";
+  }
+  return result;
+}
 
 export const ThumbnailItem = ({ node }) => {
-  const oriTtitle = node.frontmatter.title;
-  const printTitleLength = 25;
-  const prtTitle =
-    oriTtitle.length <= printTitleLength
-      ? oriTtitle
-      : oriTtitle.substr(0, printTitleLength) + "...";
+  const title = substrToByte(node.frontmatter.title, 40);
 
-  const classes = useStyles();
   return (
     <Grid xs={12} md={6} lg={4}>
       <Link
@@ -43,7 +35,7 @@ export const ThumbnailItem = ({ node }) => {
             className="thum-img"
             style={{ backgroundImage: `url(${node.frontmatter.image})` }}
           />
-          <h2>{prtTitle || node.fields.slug}</h2>
+          <h2>{title || node.fields.slug}</h2>
           <p dangerouslySetInnerHTML={{ __html: node.excerpt }}></p>
         </div>
       </Link>
